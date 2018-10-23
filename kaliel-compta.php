79c6f7a056f899693157123f6919d04d350ed153
Kaliel <maxime@kaliel.fr>
2018-10-23 18:31:02 +0200

    add menu

--- kaliel-compta.php
+++ kaliel-compta.php
@@ -10,3 +10,35 @@ License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  kaliel
 */
+
+class KalielCompta
+{
+
+    public function __construct()
+    {
+        add_menu_page(
+            'Compta',
+            'Compta',
+            'edit_posts',
+            'kaliel-compta',
+            [$this, 'output'],
+            'dashicons-analytics',
+            30
+        );
+
+    }
+
+    public function output()
+    {
+        echo '<h1>CACA</h1>';
+    }
+
+}
+
+
+function KalielCompta()
+{
+    return new KalielCompta();
+}
+
+add_action('init', 'KalielCompta');
