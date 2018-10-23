b5e5a1a7b4d4f76d75737aba6fb7fdd33d37f448
Kaliel <maxime@kaliel.fr>
2018-10-23 17:36:02 +0200

    permier commit

---
+++ kaliel-compta.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Kaliel Compta
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Export woocommerce dans un fichier csv pour compta
+Version:      1
+Author:       Kaliel
+Author URI:   kaliel.fr
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  kaliel
+*/
